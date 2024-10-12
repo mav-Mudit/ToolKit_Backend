ed24ab855f4f726385b7b4093076ed397a0c0e32
Mudit <mudittandon34@gmail.com>
2024-10-12 10:39:42 +0530

    ENV

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose');
-const cors = require('cors')
-const userController = require('./controller/user')
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const userController = require("./controller/user");
+const dotenv = require("dotenv");
+dotenv.config();
 
-const app = express()
+const app = express();
 
 app.use(cors());
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/test', (err) => {
-    if (err) {
-        console.log('DB Error')
-    } else {
-        console.log('DB Connected.')
-    }
-}); 
-app.post('/register', userController.register)
-app.post('/login', userController.login)
-app.post('/submit-otp', userController.submitotp)
-app.post('/send-otp', userController.sendotp)
+mongoose.connect(process.env.MONGO_URL_CONNECTION, (err) => {
+  if (err) {
+    console.log("DB Error");
+  } else {
+    console.log("DB Connected.");
+  }
+});
+app.post("/register", userController.register);
+app.post("/login", userController.login);
+app.post("/submit-otp", userController.submitotp);
+app.post("/send-otp", userController.sendotp);
 
-app.listen(5000, () => {
-    console.log(`Backend Running At Port 5000`)
-})
+app.listen(process.env.PORT, () => {
+  console.log(`Backend Running At Port 5000`);
+});
